08fdc4c492ff70d918d4a9946cdc97a748ba7900
Soramichi Akiyama <akiyama@m.soramichi.jp>
2019-04-23 22:20:22 +0200

    init

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -O3 convert_snap_graphmat.cpp -o conver_snap_graphmat
---
+++ convert_snap_graphmat.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <set>
+#include <map>
+
+typedef unsigned long itype;
+
+int main(int argc, char* argv[]) {
+  char buff[1024];
+  itype s, d;
+  itype min = 1e9;
+  std::set<itype> nodes = std::set<itype>();
+  std::map<itype, unsigned long> new_ids = std::map<itype, unsigned long>();
+  FILE* f;
+
+  f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("");
+  }
+
+  // 1st phase: insert node ids to a set
+  while(fgets(buff, sizeof(buff), f)) {
+    std::stringstream ss(buff);
+    ss >> s >> d;
+    nodes.insert(s);
+    nodes.insert(d);
+  }
+
+  // assign new ids to the nodes
+  unsigned long new_id = 1; // should be from 1 due to GraphMat specification
+  for(auto it = nodes.begin(); it != nodes.end(); it++) {
+    new_ids[*it] = new_id;
+    new_id++;
+  }
+  
+  // 2nd phase: convert node numbers (start and end of an edge) to their new ids
+  rewind(f);
+  while(fgets(buff, sizeof(buff), f)) {
+    std::stringstream ss(buff);
+    ss >> s >> d;    
+    printf("%ld %ld 1\n", new_ids[s], new_ids[d], 1);
+  }
+
+  return 0;
+}
